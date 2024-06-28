9d619e224367841a181e6501c7c4a2d1fd2c3b3c
Nuno Saraiva <nuno.saraiva@wacestudio.com>
2024-06-28 15:50:52 +0100

    adding tests to frontend

---
+++ frontend/src/features/CreateProof/CreateProofButton.test.tsx
@@ -0,0 +1,8 @@
+import { fireEvent, render, screen } from "@test-utils";
+import CreateProofButton from "./CreateProofButton";
+
+describe("Quote", async () => {
+  it("should render properly", async () => {
+    render(<CreateProofButton />);
+  });
+});
