afb01d709018e456852783b92dc121d08ebefd25
nsaraiva <nuno.saraiva@wacestudio.com>
2024-06-28 12:38:00 +0100

    updated verifyProof using alchemy sdk

--- frontend/src/hooks/verifyProof.ts
+++ frontend/src/hooks/verifyProof.ts
@@ -1,24 +1,21 @@
-import { useWeb3ModalProvider } from "@web3modal/ethers/react";
-import { BrowserProvider, Contract, ContractTransactionReceipt, ethers } from "ethers";
-import contract from "../../../artifacts/contracts/Provify.sol/Provify.json";
+import { Alchemy, Network } from "alchemy-sdk";
 
 const CONTRACT_ADDRESS = import.meta.env.VITE_CONTRACT_ADDRESS;
 
-const useVerifyProof = () => {
+const config = {
+  apiKey: import.meta.env.VITE_ALCHEMY_API_KEY,
+  network: Network.ETH_SEPOLIA,
+};
 
-  const { walletProvider } = useWeb3ModalProvider();
+const alchemy = new Alchemy(config);
+
+const useVerifyProof = () => {
 
   const verifyProof = async (proofId: number) => {
 
-    const provider = new ethers.JsonRpcProvider();
+    const tokenMetadata = await alchemy.nft.getNftMetadata(CONTRACT_ADDRESS, proofId);
 
-    const provifyContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, provider);
-
-    provifyContract.ad
-
-    const owner = await provifyContract.ownerOf();
-
-    console.log(owner)
+    console.log(tokenMetadata);
   }
 
   return { verifyProof };
