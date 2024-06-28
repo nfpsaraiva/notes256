5a385afd0e5757ca1c52d4a9f0aef2a20d28e1a5
nsaraiva <nuno.saraiva@wacestudio.com>
2024-06-28 10:27:26 +0100

    added tests

--- test/Provify.ts
+++ test/Provify.ts
@@ -1,16 +1,70 @@
-import {expect} from "chai";
-import {ethers} from "hardhat";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers"
 
+async function deployProvifyFixture() {
+  const [owner] = await ethers.getSigners();
 
-describe("Provify Contract", () => {
-  it("Should work", async () => {
-    const [owner] = await ethers.getSigners();
+  const provifyContract = await ethers.deployContract('Provify', owner);
 
-    const provify = await ethers.deployContract("Provify");
+  await provifyContract.waitForDeployment();
 
-    const balance = await provify.balanceOf(owner.address);
+  return { owner, provifyContract };
+}
 
-    
+describe("Create Proof", () => {
+  it("Should emit ProofCreated event", async () => {
+    const { owner, provifyContract } = await loadFixture(deployProvifyFixture)
 
-  })
-})
+    const createProof = provifyContract.createProof('foo', 'bar');
+
+    await expect(await createProof)
+      .to.emit(provifyContract, "ProofCreated")
+      .withArgs(1, 'foo', 'bar', owner.address);
+  });
+
+  it("Should emit NFTIssued event", async () => {
+    const { owner, provifyContract } = await loadFixture(deployProvifyFixture);
+
+    const createProof = provifyContract.createProof('foo', 'bar');
+
+    await expect(await createProof)
+      .to.emit(provifyContract, "NFTIssued")
+      .withArgs(1, owner.address, 1);
+  });
+
+  it("Should create 2 proofs", async () => {
+    const { owner, provifyContract } = await loadFixture(deployProvifyFixture);
+
+    await provifyContract.createProof('foo1', 'bar');
+
+    const secondProof = provifyContract.createProof('foo2', 'bar');
+
+    await expect(await secondProof)
+      .to.emit(provifyContract, "ProofCreated")
+      .withArgs(2, 'foo2', 'bar', owner.address)
+  });
+});
+
+describe("Verify Proof", () => {
+  it("Should emit ProofDetails event for 1 proof", async () => {
+    const { owner, provifyContract } = await loadFixture(deployProvifyFixture);
+
+    await provifyContract.createProof('foo', 'bar');
+
+    const verifyProof = provifyContract.verifyProof(1);
+
+    await expect(await verifyProof)
+      .to.emit(provifyContract, 'ProofDetails')
+      .withArgs(1, 'foo', 'bar', owner.address);
+  });
+
+  it("Should revert verifying a non existing proof", async () => {
+    const { provifyContract } = await loadFixture(deployProvifyFixture);
+
+    const verifyProof = provifyContract.verifyProof(1);
+
+    await expect(verifyProof)
+      .to.be.reverted;
+  });
+});
