8740f7c24cff04bb7704b4e09edb989530a6c084
Nuno Saraiva <nuno.saraiva@adclick.pt>
2024-07-20 17:30:29 +0100

    dev

--- frontend/src/components/Layout/Navbar/Menu/BottomMenu.tsx
+++ frontend/src/components/Layout/Navbar/Menu/BottomMenu.tsx
@@ -7,7 +7,7 @@ import { MenuEnum } from "@/enums";
 
 const BottomMenu: FC = () => {
   return (
-    <Stack gap={"xs"} className={classes.menu}>
+    <Stack gap={4} className={classes.menu}>
       <MenuItem name={MenuEnum.HOW_IT_WORKS} path="/how-it-works" icon={<IconQuestionMark size={20} />} />
       <MenuItem name={MenuEnum.ROADMAP} path="/roadmap" icon={<IconTimeline size={20} />} />
       <MenuItem name={MenuEnum.ABOUT} path="/about" icon={<IconTimeline size={20} />} />
