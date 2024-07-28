b12d6b641addc6b40fa77a784fdb946fee83e3c4
Nuno Saraiva <nuno.saraiva@adclick.pt>
2024-07-29 00:19:30 +0100

    fix imports

--- app/src/components/Common/Notes/NoteCardControls/NoteCardControls.tsx
+++ app/src/components/Common/Notes/NoteCardControls/NoteCardControls.tsx
@@ -1,9 +1,6 @@
 import { BlockNote, LocalNote, WebNote } from "@/types";
 import { Button, Group } from "@mantine/core";
 import { FC } from "react";
-import DeleteButton from "./DeleteButton/DeleteButton";
-import PublishButton from "./PublishButton/PublishButton";
-import { useWeb3ModalAccount } from "@web3modal/ethers/react";
 
 interface NoteCardControlsProps {
   note: LocalNote | WebNote | BlockNote,
