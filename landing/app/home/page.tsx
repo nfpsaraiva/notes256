240873d33de21396880c7b2e5c5de108e99e5d4d
Nuno Saraiva <nuno.saraiva@adclick.pt>
2024-08-01 18:24:19 +0100

    dev

---
+++ landing/app/home/page.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { AppShell } from '@/components/Layout';
+import { Center, Stack } from '@mantine/core';
+import { useScrollIntoView } from '@mantine/hooks';
+import { FC } from 'react';
+import { ContactsHero, FeaturesHero, HomeHero } from '@/components/Heros';
+
+const Home: FC = () => {
+  const {
+    scrollIntoView: scrollHome,
+    targetRef: targetHome
+  } = useScrollIntoView<HTMLDivElement>({ offset: 60, duration: 600 });
+
+  const {
+    scrollIntoView: scrollAbout,
+    targetRef: targetAbout
+  } = useScrollIntoView<HTMLDivElement>({ offset: 60, duration: 600 });
+
+  const {
+    scrollIntoView: scrollContacts,
+    targetRef: targetContacts
+  } = useScrollIntoView<HTMLDivElement>({ offset: 60, duration: 600 });
+
+  return (
+    <AppShell scrollHome={scrollHome} scrollAbout={scrollAbout} scrollContacts={scrollContacts}>
+      <Stack gap={0}>
+        <Center h={"100vh"} bg={"var(--mantine-color-gray-1)"} ref={targetHome}>
+          <HomeHero scrollAbout={scrollAbout} />
+        </Center>
+        <Center h={"100vh"} ref={targetAbout}>
+          <FeaturesHero />
+        </Center>
+        <Center h={"10vh"} ref={targetContacts}>
+          <ContactsHero />
+        </Center>
+      </Stack>
+    </AppShell>
+  );
+}
+
+export default Home;
