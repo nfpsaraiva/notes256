d98117f541f4668158f306d16e1c8e7ca24ebdc4
Nuno Saraiva <nuno.saraiva@adclick.pt>
2024-08-20 10:13:27 +0100

    dev

--- app/src/components/Layout/Navbar/Menu/BottomMenu.tsx
+++ app/src/components/Layout/Navbar/Menu/BottomMenu.tsx
@@ -4,9 +4,8 @@ import { FC } from "react";
 const BottomMenu: FC = () => {
   return (
     <Stack gap={4}>
-      <Group justify="space-between">
-        <Anchor c={"var(--mantine-color-text)"} fw={600} underline="never" target="_blank" href="https://nfpsaraiva.com" size="xs">nfpsaraiva.com</Anchor>
-        <Anchor c={"var(--mantine-color-text)"} fw={600} underline="never" target="_blank" href="https://github.com/nfpsaraiva/notes256" size="xs">v0.1.3</Anchor>
+      <Group justify="center">
+        <Anchor c={"var(--mantine-color-text)"} fw={600} underline="never" target="_blank" href="https://github.com/nfpsaraiva/notes256" size="xs">Beta v0.1.3</Anchor>
       </Group>
     </Stack>
   )
